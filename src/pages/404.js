82fb30a06c6e0c51c9c86ceeb67158c34ce0a251
pubdjerba <pubdjerba@gmail.com>
2022-12-06 16:54:33 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,34 @@
 import * as React from "react"
+import { Button, Container, Typography, Box, Stack } from "@mui/material"
+import { Link } from "gatsby"
+const NotFoundPage = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Container maxWidth="md">
+        <Stack>
+          <Box paddingBottom={3}>
+            <Typography align="center" variant="h1">
+              404
+            </Typography>
 
-const NotFoundPage = () => <h1>404: Not Found</h1>
+            <Typography align="center" variant="h6">
+              La page que vous cherchez n'existe pas.
+            </Typography>
+          </Box>
+          <Button component={Link} to={"/"} variant="contained">
+            Accueil
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
+  )
+}
 
 export default NotFoundPage
